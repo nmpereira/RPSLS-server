b629670a8d7f6f719dc8d216a891bf0695869e84
ChanelT <crene7722@gmail.com>
2022-06-02 15:49:46 -0500

    moved js into separate file

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log("Instinc is the best!");
+            const display = document.querySelector("#display");
+            document
+                .querySelector("form")
+                .addEventListener("click", async (e) => {
+                    if (e.target.classList.contains("btn")) {
+                        const id = e.target.getAttribute("id");
+                        const raw = await fetch(`/random?user=${id}`);
+                        let res = await raw.json();
+                        display.textContent = `${res.logic}, ${res.winner} wins!`;
+                    }
+                });
